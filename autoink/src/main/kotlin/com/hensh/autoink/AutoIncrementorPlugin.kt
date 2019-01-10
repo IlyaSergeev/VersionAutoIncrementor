8bd8dc70aa35d57f075e62f9ff59c7b62323e429
i-sergeev <master.hensh@gmail.com>
2019-01-10 15:31:51 +0300

    init

---
+++ autoink/src/main/kotlin/com/hensh/autoink/AutoIncrementorPlugin.kt
@@ -0,0 +1,14 @@
+package com.hensh.autoink
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by i-sergeev on 10/01/2019.
+ */
+class AutoIncrementorPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        // Логику добавим позже
+    }
+}
